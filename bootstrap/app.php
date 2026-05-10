43fcccbdd9b4f04cf6cbab52fa922b44e0b993a4
Suntsov Dmitriy <sudmal@gmail.com>
2026-05-10 12:16:06 +0300

    fix: exclude telegram webhook from CSRF

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: ['telegram/webhook']);
         $middleware->alias([
             'active' => EnsureUserIsActive::class,
         ]);
