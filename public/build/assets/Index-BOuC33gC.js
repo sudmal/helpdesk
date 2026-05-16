6077dc22985039a36aade459e1c207459ead924c
Suntsov Dmitriy <sudmal@gmail.com>
2026-05-16 14:49:47 +0300

    Settings: replace ? hint with ℹ️ emoji and tooltip

--- public/build/assets/Index-BOuC33gC.js
+++ public/build/assets/Index-BOuC33gC.js
@@ -1,4 +1,4 @@
-var Eo=Object.defineProperty;var Io=(i,t,e)=>t in i?Eo(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var P=(i,t,e)=>Io(i,typeof t!="symbol"?t+"":t,e);import{r as ut,G as zo,K as Bo,c as W,a as ds,b as Vo,h as Wo,w as No,d as y,e as Ft,v as us,o as N,F as Et,g as Gt,n as Ee,t as Y,f as oe,L as ae,B as Ho,M as fs,s as jo,q as re}from"./app-6tN59PnN.js";import{_ as $o}from"./AppLayout-CPUnZ0WK.js";/*!
+var Eo=Object.defineProperty;var Io=(i,t,e)=>t in i?Eo(i,t,{enumerable:!0,configurable:!0,writable:!0,value:e}):i[t]=e;var P=(i,t,e)=>Io(i,typeof t!="symbol"?t+"":t,e);import{r as ut,G as zo,K as Bo,c as W,a as ds,b as Vo,h as Wo,w as No,d as y,e as Ft,v as us,o as N,F as Et,g as Gt,n as Ee,t as Y,f as oe,L as ae,B as Ho,M as fs,s as jo,q as re}from"./app-Le-BLcRf.js";import{_ as $o}from"./AppLayout-Cud1GIrD.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
