33ff4356a81a5bd9fd835f31759aeade5065ebe5
Suntsov Dmitriy <sudmal@gmail.com>
2026-05-25 19:43:25 +0300

    Add API endpoints for connection requests

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ConnectionRequestController;
 use App\Http\Controllers\Api\TicketController;
 use App\Models\Material;
 use App\Models\ServiceType;
+use App\Models\Territory;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PbxController;
@@ -20,6 +22,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/tickets/{ticket}/close',         [TicketController::class, 'close']);
     Route::post('/tickets/{ticket}/attachments',   [TicketController::class, 'addAttachment']);
     Route::post('/tickets/{ticket}/reschedule',    [TicketController::class, 'reschedule']);
+
+    Route::get('/connection-requests',                              [ConnectionRequestController::class, 'index']);
+    Route::post('/connection-requests',                             [ConnectionRequestController::class, 'store']);
+    Route::get('/connection-requests/{connectionRequest}',          [ConnectionRequestController::class, 'show']);
+    Route::put('/connection-requests/{connectionRequest}',          [ConnectionRequestController::class, 'update']);
+    Route::post('/connection-requests/{connectionRequest}/close',   [ConnectionRequestController::class, 'close']);
+    Route::delete('/connection-requests/{connectionRequest}',       [ConnectionRequestController::class, 'destroy']);
 
     Route::get('/service_types', function () {
         return response()->json(
