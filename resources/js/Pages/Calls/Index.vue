64d15f8e9f168695b5be58722b8983d0ec2904ca
das <suntsov.da@gmail.com>
2026-06-29 17:22:04 +0300

    fix: add missing ivrActionBadge function in Calls/Index.vue

--- resources/js/Pages/Calls/Index.vue
+++ resources/js/Pages/Calls/Index.vue
@@ -365,6 +365,17 @@ const f = ref({
   queue_status: props.filters?.queue_status ?? '',
   ivr_action:   props.filters?.ivr_action   ?? '',
 })
+function ivrActionBadge(action) {
+  const map = {
+    new_ticket:    'bg-blue-100 text-blue-800',
+    find_ticket:   'bg-purple-100 text-purple-800',
+    check_balance: 'bg-yellow-100 text-yellow-800',
+    operator:      'bg-green-100 text-green-800',
+    no_action:     'bg-gray-100 text-gray-500',
+  }
+  return map[action] ?? 'bg-gray-100 text-gray-500'
+}
+
 function apply() {
   router.get(route('calls.index'), f.value, { preserveState: true })
 }
